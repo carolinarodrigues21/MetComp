3ae6ffdb0060825ff04162a06c4b0d65280d197d
Carolina Rodrigues <70293019+carolinarodrigues21@users.noreply.github.com>
2020-11-06 15:54:58 -0300

    Add files via upload

---
+++ Aula1/Aula1_cor/Farenheit.py
@@ -0,0 +1,12 @@
+'''
+Gabriella
+Copiou do terminal; não é programa mas os comandos estão certos.
+Sandra: 2.5
+'''
+Python 3.8.5 (tags/v3.8.5:580fbb0, Jul 20 2020, 15:43:08) [MSC v.1926 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> C = 32.5 # temperatura em Celsius
+>>> F = (C*9./5) + 32
+>>> print (F)
+90.5
+>>> 
---
+++ Aula1/Aula1_cor/gaussian.py
@@ -0,0 +1,22 @@
+'''
+ -*- encoding: utf-8 -*-
+Nessa tarefa tinha que ser o usado o python2
+Aqui voce colou as linhas do terminal, recomendo usar um editor de texto pra escrever seu programa e salva-lo com a extensao .py ,
+assim voce vai poder roda-lo depois sem ter que reescrever tudo novamente (caso voce tente rodar esse sem retirar as duas primeiras linhas e os ">>>" vai obter um erro)
+
+No python2 dividir 1/2 é diferente de 1/2., o primeiro resultado é 0, o segundo é 0.5.
+Colocar comentários no seu programa, é possivel fazê-lo num editor botando um "#" antes da linha que voce quer comentar
+exemplo:#essse programa calcula o valor de pi usando numeros aleatorio, #importando variaveis, #calculando valorda funcao, #calculando o somantorio, etc
+
+2
+'''
+Python 3.8.5 (tags/v3.8.5:580fbb0, Jul 20 2020, 15:43:08) [MSC v.1926 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> from math import *
+>>> m = 0
+>>> s = 2.
+>>> x = 1.
+>>> Gauss = 1/(sqrt(2*pi) * s) * exp(- 1/2 * ((x - m) / s)**2)
+>>> print (Gauss)
+0.17603266338214976
+>>>
