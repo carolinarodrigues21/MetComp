5d93a0fb822db280514bc062a272267892e65ace
Carolina Rodrigues <70293019+carolinarodrigues21@users.noreply.github.com>
2020-11-09 17:19:59 -0300

    Update pontosLagrange.py

--- Aula6/Aula6_cor/pontosLagrange.py
+++ Aula6/Aula6_cor/pontosLagrange.py
@@ -1,9 +1,10 @@
 '''
 O programa roda mas não fornece os resultados esperados.
-Erro: O valor das constantes G e M estão incorretos.
-Obs 1: O gráfico não mostra o zero da função pois o eixo y esta em escala logaritmica e log(0) não está definido. Além disso,
-o range do x está excessivamente extenso. Assim, a visualização do comportamento da curva em x=1.48e11 é prejudicada.
-É recomendado usar, por exemplo x = np.linspace(1.46e11,1.49e11,200).
+Erro: O valor das constantes G e M estão incorretos.    (linha 26 e 27)
+Obs 1: O gráfico não mostra o zero da função pois o eixo y esta em escala logaritmica e log(0) não está definido. (removido)
+Além disso,
+o range do x está excessivamente extenso. Assim, a visualização do comportamento da curva em x=1.48e11 é prejudicada. 
+É recomendado usar, por exemplo x = np.linspace(1.46e11,1.49e11,200). (linha 41)
 Obs 2:  Cuidado com divisões por zero. Caso o valor do chute inicial seja 0 ou 1.5e11 teriamos uma divisão por zero
 na função e sua derivada.
 '''
@@ -22,8 +23,8 @@ import numpy as np
 
 print("Esse programa implementa o método Newton-Raphson de para achar os pontos de Lagrange do sistema Terra-Sol")
 
-G = 6.614 * 10**-11       #Constante gravitacional
-M = 1.89 * 10**30         #Massa do Sol
+G = 6.674 * 10**-11       #Constante gravitacional
+M = 1.989 * 10**30        #Massa do Sol
 m = 5.9736 * 10**24       #Massa da Terra
 R = 1.5 * 10**11          #Distância entre a Terra e o Sol
 omega = 1.992 * 10**-7    #Velocidade angular da Terra e do Satélite
@@ -37,10 +38,9 @@ def derivada(r):
   return -G*2*M/r**3 - G*2*m/(R-r)**3 - omega**2
 
 #faz o gráfico da função acima com os valores de x e y definidos
-x = np.linspace(0,R,200)
+x = np.linspace(1.46e11,1.49e11,200)
 y = f(x)
 plt.plot(x,y)
-plt.yscale('log')
 plt.grid(True)
 plt.show()
 
